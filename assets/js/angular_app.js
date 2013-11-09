b070c4176fcaffc39b7ce81688eb260df5028a4e
Johnny Rahajarison <rahajarison.johnny@gmail.com>
2013-11-09 13:54:51 +0100

    Begin working on admin interface

--- assets/js/angular_app.js
+++ assets/js/angular_app.js
@@ -3,3 +3,7 @@ var app = angular.module('fetcher', []);
 app.controller('homePageCtrl', function homePageCtrl($scope) {
   $scope.toto = 'yeah'
 });
+
+app.controller('adminPageCtrl', function adminPageCtrl($scope) {
+
+});
